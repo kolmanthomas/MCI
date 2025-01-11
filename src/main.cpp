f9936848d3ffce37e47b2b7e93a0d06f050559c9
kolmanthomas <realkolmanthomas@gmail.com>
2025-01-11 13:44:37 -0500

    Adding main.cpp for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,75 @@
 #include <iostream>
-#include <random>
-
 #include <vector>
-#include <boost/program_options.hpp>
+#include <cmath>
 
-namespace po = boost::program_options;
+#include <boost/math/quadrature/naive_monte_carlo.hpp>
+
+#include "simdxorshift128plus.h"
+
+#include "mci.hpp"
+
+float f(float x) {
+    return std::sqrt(1 - x * x);
+}
 
 /*
 * Monte Carlo Estimator
 */
+
 int main(int argc, const char **argv)
-{
+{ 
+    /*
+    auto g = [](std::vector<double> const & x)
+    {
+        //constexpr const double A = 1.0 / (M_PI * M_PI * M_PI);
+        return std::sqrt(9 - x[0] * x[0]);
+    };
+    std::vector<std::pair<double, double>> bounds{{-3, 3}};
+    double error_goal = 0.001;
+    boost::math::quadrature::naive_monte_carlo<double, decltype(g)> mc(g, bounds, error_goal);
+    std::future<double> task = mc.integrate();
 
-    std::random_device rd;
-    std::mt19937 rng(rd());
-    std::uniform_real_distribution<> dist(-1, 1);
-
-
-    std::vector<std::pair<float, float>> m;
-    int count = 0;
-    int runs = 100000;
-    for (int i = 0; i < runs; i++) {
-        std::pair pair(dist(rd), dist(rd));
-        m.push_back(pair);
-        if ((pair.first*pair.first + pair.second*pair.second) < 1) {
-            count += 1; 
+    while (task.wait_for(std::chrono::seconds(1)) != std::future_status::ready)
+    {
+        // The user must decide on a reasonable way to display the progress depending on their environment:
+        display_progress(mc.progress(),
+                         mc.current_error_estimate(),
+                         mc.current_estimate(),
+                         mc.estimated_time_to_completion());
+        if (some_signal_heard()
+        {
+            mc.cancel();
+            std::cout << "\nCancelling because this is too slow!\n";
         }
-        std::cout << "First value: " << m[i].first << ", and second value: " << m[i].second << std::endl;
     }
+    double y = task.get();
+    std::cout << "Integral: " << y << std::endl;
 
-    // Ratio of points within circle to 
-    float ratio = float(count) / float(runs);
+    auto integrand = [](float x)
+    {
+        return std::sqrt(1 - x * x);
+    };
+    */
 
-    // Area of circle
-    std::cout << "Area of the circle is: " << ratio * 4 << std::endl;
+    float (*integrand)(float);
+    integrand = &f;
+
+    // Timing routine
+    // std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
 
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+    //mci(integrand, {{-1, 1}}, 262144);
+    float res = mci(integrand, {{-1, 1}}, 16777216);
+    // std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    // auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    // std::cout << "Time     // Area of circle
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Time elapsed: " << elapsed.count() / 1000000.0f << " seconds " << std::endl;
+    std::cout << "Area of the circle is: " << res * 2 << std::endl;
 }
 
 
